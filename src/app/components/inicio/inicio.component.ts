52ba91eaf7f1144741d25e6b3636f3eb95edc470
miryambathilde <mbathilde@gmail.com>
2021-05-10 11:55:02 +0200

    altura dinamico + metodo cambiarAltura

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -10,9 +10,15 @@ export class InicioComponent implements OnInit {
   //variables edad y peso dinamico
   edad = 25;
   peso = 60;
+  altura = 160;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /* metodo cambiarAltura */
+  cambiarAltura(event: any){
+    this.altura = event.target.value;
   }
 
 }
